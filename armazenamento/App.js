6d1c2f7746d6f5cff3484f8c406fa14134bc0c50
MarsoL4 <enzogmarsola@gmail.com>
2025-04-10 09:47:07 -0300

    feat: Async Storage

--- armazenamento/App.js
+++ armazenamento/App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, TextInput } from 'react-native-web';
 
 export default function App() {
+  const key = '2TDSPK-2025'
+  const [valorInput, alteraValorInput] = useState('')
+  const save = () => {
+    saveData(key, valorInput);
+  }
+
+  const saveData = async (key, value) => {
+    try {
+      await AsyncStorage.setItem(key, value);
+    } catch (e) {
+      // saving error
+      console.log('[Error][saveData]');
+    }
+  };
+
+  const leData = async (key) => {
+    return AsyncStorage.getItem(key)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput value={valorInput} onChangeText={alteraValorInput}></TextInput>
+      <Button title='Salvar' onPress={save}>
+      </Button>
+      <Button title='Ler' onPress={() => {}}></Button>
     </View>
   );
 }
